8edbc22252562821d72513a70158cb77913986d2
Jacob Brewbaker <brewbakerjacob@gmail.com>
2024-06-09 19:18:09 -0400

    fixed knockback

--- Characters/Reusable Scripts/Knockback.gd
+++ Characters/Reusable Scripts/Knockback.gd
@@ -3,9 +3,13 @@ extends Node
 class_name Knockback
 
 var taking_hit_knockback : int
+var can_knockback : bool = true
 
 @export var charcter : CharacterBody2D
+@export var knockback_timer : Timer
 
+func _ready():
+	knockback_timer.connect("timeout", Callable(self, "_can_get_knockback"))
 
 func grab_attack_direction_position(chosen_opponent):
 	var opponent_position = chosen_opponent.global_position
@@ -24,9 +28,14 @@ func apply_force_to_charcter(opponent, force):
 	var attack_direction = set_attack_direction(attack_position)
 	#charcter.movement_lock = true 
 	var knockback = attack_direction * force
-	if charcter.is_on_floor():
-		charcter.velocity.x += knockback
-		charcter.move_and_slide()
+	if can_knockback:
+		if charcter.is_on_floor():
+			charcter.velocity.x += knockback
+			charcter.move_and_slide()
+			can_knockback = false
+			knockback_timer.start()
 	#charcter.movement_lock = false 
 		
+func _can_get_knockback():
+	can_knockback = true
 ##function that given direction and applies knockback##
